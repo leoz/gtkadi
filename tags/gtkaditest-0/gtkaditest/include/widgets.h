c77ba40206360364a3c0640fea5af4036724645c
nobody <nobody@d0de6190-4211-0410-974d-aad99ff7acbb>
2005-10-03 20:52:42 +0000

    This commit was manufactured by cvs2svn to create tag 'gtkaditest-0'.

---
+++ tags/gtkaditest-0/gtkaditest/include/widgets.h
@@ -0,0 +1,25 @@
+/* GTK ADI Test
+ * widgets.h
+ * Copyright (C) 2003 - 2005, Leonid Zolotarev <leonid.zolotarev@gmail.com>
+ *
+ * Licensed under the terms of the BSD license, see file COPYING
+ * for details.
+ *
+ * $Id$
+ */
+#ifndef __WIDGETS_H__
+#define __WIDGETS_H__
+
+#include <gtk/gtk.h>
+
+gchar* get_gtk_adi_version_string (void);
+
+void set_main_window_title (GtkWidget *main_window);
+
+GtkWidget* create_adi (void);
+
+GtkWidget* create_toolbar (void);
+
+GtkWidget* create_menubar (void);
+
+#endif /* __WIDGETS_H__ */
---
+++ tags/gtkaditest-0/gtkaditest/include/window.h
@@ -0,0 +1,15 @@
+/* GTK ADI Test
+ * window.h
+ * Copyright (C) 2003 - 2005, Leonid Zolotarev <leonid.zolotarev@gmail.com>
+ *
+ * Licensed under the terms of the BSD license, see file COPYING
+ * for details.
+ *
+ * $Id$
+ */
+#ifndef __WINDOW_H__
+#define __WINDOW_H__
+
+GtkWidget* create_main_window (void);
+
+#endif /* __WINDOW_H__ */
