ee64ee02d152490fa91cb6948f929a59ac869e69
zoleo <zoleo@d0de6190-4211-0410-974d-aad99ff7acbb>
2005-10-03 21:39:00 +0000

    Initial revision

---
+++ trunk/web/frames/head.php
@@ -0,0 +1,11 @@
+<head>
+	<link href="style.css" rel="stylesheet" type="text/css"/>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+	<link rel="icon" href="favicon.ico" type="image/x-icon"/>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
+	<meta name="generator" content="LeoZ"/>
+	<meta name="description" content="GTK ADI Library is a GTK widget library which implements an ADI widget framework"/>
+	<meta name="keywords" content="gtkadi, GTK, ADI, MDI, widget, library"/>
+	<meta name="author" content="Leonid Zolotarev"/>
+	<title>GTK ADI Library</title>
+</head>
---
+++ trunk/web/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+          "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html lang="en">
+<?php include("frames/head.php"); ?>
+<body>
+<?php include("frames/header.php"); ?>
+<div id="container">
+<div id="content">
+<?php include("frames/left_bar.php"); ?>
+<div id="main">
+
+<h2>News</h2>
+
+<p>
+2005-10-04: Initial web page setup. No files are published.
+Use CVS to get latest sources.
+</p>
+
+</div>	
+</div>
+</div>
+<div class="clearing"> </div>
+<div id="footer">
+	&copy; 2005 Leonid Zolotarev. All rights reserved.
+	<br/>
+	$Id$
+</div>
+</body>
+</html>
