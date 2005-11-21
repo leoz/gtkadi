3c784ff7ff424d3173f801895aee6fe450a3ca00
Leonid Zolotarev <1255921+leoz@users.noreply.github.com>
2005-11-21 21:02:07 +0000

    Prepare for release 0.1.

---
+++ web/frames/download.php
@@ -0,0 +1,6 @@
+<div id="contents">
+<ul>
+<li><a href="http://prdownloads.sourceforge.net/gtkadi/gtkadi-0.1.0.tar.gz?download">gtkadi-0.1.0</a></li>
+<li><a href="http://prdownloads.sourceforge.net/gtkadi/gtkaditest-0.1.0.tar.gz?download">gtkaditest-0.1.0</a></li>
+</ul>
+</div>
