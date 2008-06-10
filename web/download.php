a088dea36aefd1deaf536d9f3ac89678a8479666
Sudarsana Babu Nagineni <sudarsana.babu@mapbox.com>
2008-06-10 07:49:22 +0000

    Creating a new branch

--- web/download.php
+++
@@ -1,6 +0,0 @@
-<div id="contents">
-<ul>
-<li><a href="http://prdownloads.sourceforge.net/gtkadi/gtkadi-0.1.3.tar.gz?download">gtkadi-0.1.3</a></li>
-<li><a href="http://prdownloads.sourceforge.net/gtkadi/gtkaditest-0.1.0.tar.gz?download">gtkaditest-0.1.0</a></li>
-</ul>
-</div>
